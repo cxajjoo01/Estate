4e7076fafb52f71cf610ef95fc4ae0cfca741af1
Ajay <cxajjoo01@gmail.com>
2024-06-23 09:12:28 +0545

    create MongoDB connection and run

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(7000, ()=>{
-    console.log("Server is running on port 7000!!");
-})
+mongoose
+  .connect(process.env.MongoDB)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000!!");
+});
