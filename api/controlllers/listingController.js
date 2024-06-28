39fc9826abdf2470d405986b46227cdd234d5698
Ajay <cxajjoo01@gmail.com>
2024-06-28 21:44:33 +0545

    create update listing  api route

--- api/controlllers/listingController.js
+++ api/controlllers/listingController.js
@@ -1,6 +1,7 @@
 import Listing from "../models/listingModel.js";
 import { errorHandler } from "../utils/error.js";
 
+
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -12,7 +13,7 @@ export const createListing = async (req, res, next) => {
 
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
-  
+
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
   }
@@ -28,3 +29,25 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async(req,res,next) =>{
+
+  const listing = await Listing.findById(req.params.id)
+
+  if(!listing) return next(errorHandler(404, "Listing not found!"));
+
+  if(req.user.id !== listing.userRef){
+    return next(errorHandler(401, "You are not authorized to update this listing!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,10 +1,11 @@
 import { verifyToken } from "../utils/verifyUserToken.js";
 import express from 'express';
-import { createListing,deleteListing } from "../controlllers/listingController.js";
+import { createListing,deleteListing ,updateListing} from "../controlllers/listingController.js";
 
 const router = express.Router()
 
 router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id', verifyToken,deleteListing)
+router.post('/update/:id', verifyToken,updateListing)
 
 export default router;
